302014629019211666bec917500e4bcaaae4d38b
viewtifult <tashanduncan1994@gmail.com>
2021-01-26 19:59:04 +0000

    add template parts and page templates

--- template-gg.php
+++ template-gg.php
@@ -1,8 +1,20 @@
-<?php get_header();?>
+<?php
+
+/*
+Template Name: GG
+*/
+?>
+
+<?php get_header('gg');?>
 
 <div class="container">
 
     <h1><?php the_title();?></h1>
+
+        <div class="row">
+        
+        
+        </div>
 
     <?php get_template_part('includes/section', 'content');?>
 
